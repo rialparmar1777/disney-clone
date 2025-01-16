4a6bc884038d29a94a88e1d0b1bef021c6b78676
Rial <rialparmar007@gmail.com>
2025-01-15 16:51:58 -0800

    working on Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,14 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import Header from "./Components/Header";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <div className="App">
-      <h1 class="text-3xl font-bold underline">Hello world!</h1>{" "}
+    <div className="">
+      <Header />
     </div>
   );
 }
