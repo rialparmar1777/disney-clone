cab45172d6a27c8ce63392eb0298962379275957
Rial <rialparmar007@gmail.com>
2025-01-19 00:28:37 -0800

    Starting with the main movie cards

--- src/Components/GenreMovieList.jsx
+++ src/Components/GenreMovieList.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import GenresList from '../Constant/GenresList'
+import MovieList from './MovieList'
 
-const GenreMovieList = () => {
+function GenreMovieList() {
   return (
     <div>
-        GenreMovieList
+        {GenresList.genere.map((item,index)=>index<=4&&(
+            <div className='p-8 px-8 md:px-16'>
+                <h2 className='text-[20px] text-white 
+                font-bold'>{item.name}</h2> 
+                <MovieList genreId={item.id} index_={index} />   
+            </div>
+        ))}
     </div>
   )
 }
