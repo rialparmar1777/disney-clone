fdac1c16664e91bde18d7e24db3267412252c04d
Rial <rialparmar007@gmail.com>
2025-01-19 12:29:16 -0800

    Trying to show movie cards according to genres

--- src/Components/MovieCard.jsx
+++ src/Components/MovieCard.jsx
@@ -1,15 +1,16 @@
-import React from 'react'
-const IMAGE_BASE_URL="https://image.tmdb.org/t/p/original";
+import React from 'react';
+const IMAGE_BASE_URL = 'https://image.tmdb.org/t/p/original';
 
-function MovieCard({movie}) {
+function MovieCard({ movie }) {
   return (
-    <>
-        <img src={IMAGE_BASE_URL+movie.poster_path} 
-        className='w-[110px] md:w-[200px] rounded-lg
-        hover:border-[3px] border-gray-400 cursor-pointer
-        hover:scale-110 transition-all duration-150 ease-in'/>
-    </>
-  )
+    <div className="movie-card">
+      <img
+        src={IMAGE_BASE_URL + movie.poster_path}
+        alt={movie.title}
+        className="movie-card-image"
+      />
+    </div>
+  );
 }
 
-export default MovieCard
+export default MovieCard;
--- src/Components/MovieList.jsx
+++ src/Components/MovieList.jsx
@@ -1,51 +1,58 @@
-import React, { useEffect, useRef, useState } from 'react'
-import GlobalApi from '../Services/GlobalApi'
+import React, { useEffect, useRef, useState } from 'react';
+import GlobalApi from '../Services/GlobalApi';
 import MovieCard from './MovieCard';
-import { IoChevronBackOutline, IoChevronForwardOutline } from 'react-icons/io5';
 import HrMovieCard from './HrMovieCard';
-function MovieList({genreId,index_}) {
-    const [movieList,setMovieList]=useState([])
-    const elementRef=useRef(null);
-    useEffect(()=>{
-        getMovieByGenreId();
-    },[])
+import { IoChevronBackOutline, IoChevronForwardOutline } from 'react-icons/io5';
 
-    const getMovieByGenreId=()=>{
-        GlobalApi.getMovieByGenreId(genreId).then(resp=>{
-            // console.log(resp.data.results)
-            setMovieList(resp.data.results)
-        })
-    }
+function MovieList({ genreId, index_ }) {
+  const [movieList, setMovieList] = useState([]);
+  const elementRef = useRef(null);
 
-    const slideRight=(element)=>{
-        element.scrollLeft+=500;
-    }
-    const slideLeft=(element)=>{
-        element.scrollLeft-=500;
-    }
+  useEffect(() => {
+    fetchMoviesByGenre();
+  }, []);
+
+  const fetchMoviesByGenre = () => {
+    GlobalApi.getMovieByGenreId(genreId).then((response) => {
+      setMovieList(response.data.results);
+    });
+  };
+
+  const slideRight = () => {
+    elementRef.current.scrollLeft += 500;
+  };
+
+  const slideLeft = () => {
+    elementRef.current.scrollLeft -= 500;
+  };
+
   return (
-    <div className='relative'>
-         <IoChevronBackOutline onClick={()=>slideLeft(elementRef.current)} 
-         className={`text-[50px] text-white
-           p-2 z-10 cursor-pointer 
-            hidden md:block absolute
-            ${index_%3==0?'mt-[80px]':'mt-[150px]'} `}/>
-   
-    <div ref={elementRef} className='flex overflow-x-auto gap-8
-     scrollbar-none scroll-smooth pt-4 px-3 pb-4'>
-        {movieList.map((item,index)=>(
-           <>
-          {index_%3==0?<HrMovieCard movie={item}/>:<MovieCard movie={item} />}
-           </> 
+    <div className="movie-list-container">
+      <IoChevronBackOutline
+        onClick={slideLeft}
+        className={`slider-arrow left ${
+          index_ % 3 === 0 ? 'arrow-low' : 'arrow-high'
+        }`}
+      />
+      <div ref={elementRef} className="movie-list">
+        {movieList.map((item, index) => (
+          <React.Fragment key={item.id}>
+            {index_ % 3 === 0 ? (
+              <HrMovieCard movie={item} />
+            ) : (
+              <MovieCard movie={item} />
+            )}
+          </React.Fragment>
         ))}
+      </div>
+      <IoChevronForwardOutline
+        onClick={slideRight}
+        className={`slider-arrow right ${
+          index_ % 3 === 0 ? 'arrow-low' : 'arrow-high'
+        }`}
+      />
     </div>
-    <IoChevronForwardOutline onClick={()=>slideRight(elementRef.current)}
-           className={`text-[50px] text-white hidden md:block
-           p-2 cursor-pointer z-10 top-0
-            absolute right-0 
-            ${index_%3==0?'mt-[80px]':'mt-[150px]'}`}/> 
-    </div>
-  )
+  );
 }
 
-export default MovieList
+export default MovieList;
