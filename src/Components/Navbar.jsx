36e28b2db1b4a3fd67283a2dec55a347968c5833
Rial <rialparmar007@gmail.com>
2025-01-20 02:30:16 -0800

    Enhanced few stylings majorly Navbar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,33 +1,74 @@
-import React, { useState } from 'react';
-import './Navbar.css';
-import logo from '../assets/Images/logo.png';
-import { FaHome, FaSearch, FaPlus, FaFilm, FaTv, FaStar, FaUser, FaBars } from 'react-icons/fa';
+import React, { useState } from "react";
+import "./Navbar.css";
+import logo from "../assets/Images/logo.png";
+import {
+  FaHome,
+  FaSearch,
+  FaPlus,
+  FaFilm,
+  FaTv,
+  FaStar,
+  FaUser,
+  FaBars,
+  FaCog,
+  FaSignOutAlt
+} from "react-icons/fa";
 
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
+  const [profileOpen, setProfileOpen] = useState(false);
 
   const toggleMenu = () => {
     setIsOpen(!isOpen);
+  };
+
+  const toggleProfile = () => {
+    setProfileOpen(!profileOpen);
   };
 
   return (
     <nav className="navbar">
       <div className="navbar-left">
         <img src={logo} alt="Disney Logo" className="logo" />
-        <div className={`navbar-items ${isOpen ? 'open' : ''}`}>
-          <div className="navbar-item"><FaHome /> Home</div>
-          <div className="navbar-item"><FaSearch /> Search</div>
-          <div className="navbar-item"><FaPlus /> Watchlist</div>
-          <div className="navbar-item"><FaFilm /> Movies</div>
-          <div className="navbar-item"><FaTv /> Series</div>
-          <div className="navbar-item"><FaStar /> Originals</div>
-        </div>
-      </div>
-      <div className="navbar-right">
-        <FaUser /> Profile
       </div>
       <div className="navbar-toggle" onClick={toggleMenu}>
         <FaBars />
+      </div>
+      <div className={`navbar-items ${isOpen ? "open" : ""}`}>
+        <div className="navbar-item">
+          <FaHome /> Home
+        </div>
+        <div className="navbar-item">
+          <FaSearch /> Search
+        </div>
+        <div className="navbar-item">
+          <FaPlus /> Watch List
+        </div>
+        <div className="navbar-item">
+          <FaStar /> Originals
+        </div>
+        <div className="navbar-item">
+          <FaFilm /> Movies
+        </div>
+        <div className="navbar-item">
+          <FaTv /> Series
+        </div>
+      </div>
+      <div className="navbar-right">
+        <div className="profile" onClick={toggleProfile}>
+          <FaUser className="profile-icon" />
+          <div className={`profile-dropdown ${profileOpen ? "open" : ""}`}>
+            <div className="profile-item">
+              <FaUser /> My Account
+            </div>
+            <div className="profile-item">
+              <FaCog /> Settings
+            </div>
+            <div className="profile-item">
+              <FaSignOutAlt /> Log Out
+            </div>
+          </div>
+        </div>
       </div>
     </nav>
   );
