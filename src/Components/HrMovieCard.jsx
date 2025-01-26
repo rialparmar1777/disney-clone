ed5531e0f1067c0047dacaedf349576def974e17
Rial <rialparmar007@gmail.com>
2025-01-25 23:38:55 -0800

    styling hrmoviecard

--- src/Components/HrMovieCard.jsx
+++ src/Components/HrMovieCard.jsx
@@ -5,11 +5,14 @@ import "./HrMovieCard.css";
 function HrMovieCard({ movie }) {
   return (
     <div className="hr-movie-card">
-      <img
-        src={IMAGE_BASE_URL + movie.backdrop_path}
-        alt={movie.title}
-        className="hr-card-image"
-      />
+      <div className="hr-card-image-wrapper">
+        <img
+          src={IMAGE_BASE_URL + movie.backdrop_path}
+          alt={movie.title}
+          className="hr-card-image"
+        />
+        <div className="hr-card-overlay"></div>
+      </div>
       <h3 className="hr-card-title">{movie.title}</h3>
     </div>
   );
